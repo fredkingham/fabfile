68036aa46917e31d82a5481437d12bdb55a98e75
fredkingham <fredkingham@gmail.com>
2013-03-25 17:42:43 +0000

    changed to download and use brunch with my brunch skeleton

--- fabfile.py
+++ fabfile.py
@@ -8,6 +8,14 @@ VIRTUAL_ENV_WRAPPER = "/usr/local/bin/virtualenvwrapper.sh"
 """requires virtualenvwrapper/virtualenv/pip/git sets up a virtual env with ipython and django and creates the project"""
 
 
+class Project(name):
+    def __init__(name):
+        self.project_dir = "%s_project" % name
+        self.application_dir = os.path.join(self.project_dir, name)
+        self.front_end_dir = os.path.join(self.application_dir, "front_end")
+        self.app_name = names
+
+
 def require_variable(var_name):
     """ check if a variable exists in the environment """
     if var_name not in os.environ:
@@ -15,8 +23,6 @@ def require_variable(var_name):
         sys.exit(-1)
 
 #we need to move to our script directory
-#if we need to create
-
 
 def require_function(func_name):
     if not function_exists(func_name):
@@ -30,6 +36,11 @@ def function_exists(func_name):
 def get_project_name(name):
     return "%s_project" % name
 
+def setup_front_end(project):
+    with lcd(project.front_end):
+        local("npm install brunch")
+        local("node_modules/brunch/bin/brunch new static --skeleton https://github.com/fredkingham/brunch-with-cantaloupe.git")
+
 def project_setup(name, init_git, empty, requirements_in):
     if not empty: 
         if requirements_in:
@@ -40,15 +51,19 @@ def project_setup(name, init_git, empty, requirements_in):
             local("pip install ipdb")
             local("pip install mock")
 
+    project = Project(name)
 
-    project_name = get_project_name()
-    local("django-admin.py startproject --template=https://github.com/fredkingham/django-simple-project/archive/master.zip %s" % name)
-    local("mv %s %s" % (name, project_name))
+    local("django-admin.py startproject --template=https://github.com/fredkingham/django-project-skeleton/archive/master.zip %s" % name)
+    local("mv %s %s" % (name, project.project_name)
 
-    with lcd(project_name):
+    setup_front_end(project)
+
+    with lcd(project.project_name):
         local("pip freeze > requirements.txt")
         if init_git:
             local("git init")
+
+
 
 @task
 def create(name, init_git = True, empty=False, requirements_in=False):
@@ -56,6 +71,7 @@ def create(name, init_git = True, empty=False, requirements_in=False):
     require_function("pip")
     require_function("python")
     require_function("git")
+    require_function("npm")
 
     with prefix("source %s" % VIRTUAL_ENV_WRAPPER):
         with settings(warn_only=True):
