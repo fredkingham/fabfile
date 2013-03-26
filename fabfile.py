ad18c77bc3c4a1970e77aea521a1e2b8dfdc1e0e
fredkingham <fredkingham@gmail.com>
2013-03-26 09:47:54 +0000

    splitting up different functionality adding tasks

--- fabfile.py
+++ fabfile.py
@@ -8,12 +8,12 @@ VIRTUAL_ENV_WRAPPER = "/usr/local/bin/virtualenvwrapper.sh"
 """requires virtualenvwrapper/virtualenv/pip/git sets up a virtual env with ipython and django and creates the project"""
 
 
-class Project(name):
-    def __init__(name):
+class Project(object):
+    def __init__(self, name):
         self.project_dir = "%s_project" % name
         self.application_dir = os.path.join(self.project_dir, name)
         self.front_end_dir = os.path.join(self.application_dir, "front_end")
-        self.app_name = names
+        self.app_name = name
 
 
 def require_variable(var_name):
@@ -22,7 +22,6 @@ def require_variable(var_name):
         sys.stderr.write("var_name %s not found.\n\n" % var_name)
         sys.exit(-1)
 
-#we need to move to our script directory
 
 def require_function(func_name):
     if not function_exists(func_name):
@@ -33,46 +32,37 @@ def require_function(func_name):
 def function_exists(func_name):
     return bool(local("which %s" % func_name, capture=True))
 
-def get_project_name(name):
-    return "%s_project" % name
 
 def setup_front_end(project):
-    with lcd(project.front_end):
+    with lcd(project.front_end_dir):
         local("npm install brunch")
         local("node_modules/brunch/bin/brunch new static --skeleton https://github.com/fredkingham/brunch-with-cantaloupe.git")
 
-def project_setup(name, init_git, empty, requirements_in):
-    if not empty: 
-        if requirements_in:
-            local("pip install -r %s" % requirements_in)
-        else:
-            local("pip install ipython")
-            local("pip install django")
-            local("pip install ipdb")
-            local("pip install mock")
 
-    project = Project(name)
-
-    local("django-admin.py startproject --template=https://github.com/fredkingham/django-project-skeleton/archive/master.zip %s" % name)
-    local("mv %s %s" % (name, project.project_name)
-
-    setup_front_end(project)
-
-    with lcd(project.project_name):
-        local("pip freeze > requirements.txt")
-        if init_git:
-            local("git init")
-
+@task
+def django_project_setup(name, init_git=True):
+    with prefix("source %s" % VIRTUAL_ENV_WRAPPER):
+        with prefix("workon %s" % name):
+            project = Project(name)
+            local("django-admin.py startproject --template=https://github.com/fredkingham/django-project-skeleton/archive/master.zip %s" % name)
+            local("mv %s %s" % (name, project.project_dir))
+            setup_front_end(project)
+            with lcd(project.project_dir):
+                local("pip freeze > requirements.txt")
+                if init_git:
+                    local("git init")
 
 
 @task
-def create(name, init_git = True, empty=False, requirements_in=False):
-    require_variable("VIRTUALENVWRAPPER_HOOK_DIR")
-    require_function("pip")
-    require_function("python")
-    require_function("git")
-    require_function("npm")
+def init_git(name):
+    project = Project(name)
+    with lcd(project.project_dir):
+        local("pip freeze > requirements.txt")
+        local("git init")
 
+
+@task
+def virtual_env_setup(name):
     with prefix("source %s" % VIRTUAL_ENV_WRAPPER):
         with settings(warn_only=True):
             virtual_envs = local("workon", capture=True).splitlines()
@@ -82,15 +72,37 @@ def create(name, init_git = True, empty=False, requirements_in=False):
     with prefix("source %s" % VIRTUAL_ENV_WRAPPER):
         local("mkvirtualenv --no-site-packages %s" % name)
 
-    with prefix("source %s" % VIRTUAL_ENV_WRAPPER):
-        with prefix("workon %s" % name):
-            project_setup(name, init_git, empty, requirements_in)
+
+@task
+def install_common_components(requirements_in=None):
+    if requirements_in:
+        local("pip install -r %s" % requirements_in)
+    else:
+        local("pip install ipython")
+        local("pip install django")
+        local("pip install ipdb")
+        local("pip install mock")
+
+
+@task
+def create(name, init_git=True, empty=False, requirements_in=False):
+    require_variable("VIRTUALENVWRAPPER_HOOK_DIR")
+    require_function("pip")
+    require_function("python")
+    require_function("git")
+    require_function("npm")
+    virtual_env_setup(name)
+
+    if not empty:
+        install_common_components(name)
+
+    django_project_setup(name, init_git)
+
 
 @task
 def remove(name):
     with prefix("source %s" % VIRTUAL_ENV_WRAPPER):
         with settings(warn_only=True):
             local("rmvirtualenv %s" % name)
-            local("rm -rf %s" % get_project_name(name))
-
-
+            project = Project(name)
+            local("rm -rf %s" % project.project_dir)
